806bf179a04a478edeb27fcd7aac27b1211e2715
ben182 <moin@benjaminbortels.de>
2018-03-01 21:29:51 +0100

    Install

--- get.sh
+++ get.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/ben182/server-tool.git /etc/server-tool
-exec /etc/server-tool/install.sh
+chmod +x /etc/server-tool/install.sh
+chmod +x /etc/server-tool/vhost.sh
+bash /etc/server-tool/install.sh
