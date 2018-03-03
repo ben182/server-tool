576de0f956980a52bc7c9c61972a07b565ae70e2
ben182 <moin@benjaminbortels.de>
2018-03-03 23:49:14 +0100

    ssh

--- install.sh
+++ install.sh
@@ -94,6 +94,7 @@ sudo mkdir -m 0700 /var/www/.ssh
 sudo chown -R www-data:www-data /var/www/.ssh
 sudo -Hu www-data ssh-keygen -f "/var/www/.ssh/id_rsa" -t rsa -b 4096 -N ''
 ssh-keyscan github.com >> /var/www/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 SSH_KEY=$(cat /var/www/.ssh/id_rsa.pub)
 sudo sed -i "s|GITHUB_SSH|$SSH_KEY|" ${ABSOLUTE_PATH}config.json
