cf043d06980d35d44c86dd7b66ec958b4751bbd0
ben182 <moin@benjaminbortels.de>
2018-03-04 15:17:49 +0100

    vhost

--- app/Http/Helper.php
+++ app/Http/Helper.php
@@ -14,3 +14,10 @@ function replace_string_in_file($filename, $string_to_replace, $replace_with){
     $content=implode($replace_with, $content_chunks);
     file_put_contents($filename, $content);
 }
+
+function apache_permissions() {
+    echo shell_exec('chown -R www-data:www-data /var/www 2>&1');
+    echo shell_exec('chmod -R 755 /var/www 2>&1');
+    echo shell_exec('chmod g+s /var/www 2>&1');
+    echo shell_exec('chmod -R 700 /var/www/.ssh 2>&1');
+}
--- routes/console.php
+++ routes/console.php
@@ -57,4 +57,45 @@ Artisan::command('vhost:add', function () {
     copy(templates_path() . 'apache/vhost.conf', "/etc/apache2/sites-available/$sDomain.conf");
 
     replace_string_in_file("/etc/apache2/sites-available/$sDomain.conf", 'DOCUMENT_ROOT', $sDomain);
+
+    $bWwwAlias = $this->confirm('www Alias?', 1);
+
+    if (!$bWwwAlias) {
+        replace_string_in_file("/etc/apache2/sites-available/$sDomain.conf", 'ServerAlias www.SERVER_NAME', '');
+    }
+
+    replace_string_in_file("/etc/apache2/sites-available/$sDomain.conf", 'SERVER_NAME', $sDomain);
+    replace_string_in_file("/etc/apache2/sites-available/$sDomain.conf", 'NAME', $sDomain);
+
+    echo shell_exec("a2ensite $DOMAIN.conf 2>&1");
+
+    mkdir("/var/www/$DOMAIN/html", 755, TRUE);
+
+    $bSsl = $this->confirm('SSL?', 1);
+
+    if ($bSsl) {
+        $sEmail = $this->ask('Email?');
+
+        echo shell_exec("certbot --non-interactive --agree-tos --email $sEmail --apache --domains $sDomain 2>&1");
+    }
+
+    $sHtaccess = $this->choice('htaccess?', ['Non SSL to SSL and www to non www', 'www to non www', 'Nothing']);
+
+    switch ($sHtaccess) {
+        case 'Non SSL to SSL and www to non www':
+
+            copy(templates_path() . 'apache/nonSSL_to_SSL_and_www_to_nonwww.htaccess', "/var/www/$DOMAIN/html/.htaccess");
+            break;
+
+        case 'www to non www':
+
+            copy(templates_path() . 'apache/www_to_nonwww.htaccess', "/var/www/$DOMAIN/html/.htaccess");
+            break;
+
+        default:
+            break;
+    }
+
+    apache_permissions();
+    echo shell_exec('service apache2 reload 2>&1');
 });
