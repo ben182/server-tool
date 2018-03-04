bcd1b08b0a2715f7cb56957b20391fbcaede04f8
ben182 <moin@benjaminbortels.de>
2018-03-04 23:53:11 +0100

    app install

--- app/Console/Commands/ApplicationInstall.php
+++ app/Console/Commands/ApplicationInstall.php
@@ -50,6 +50,6 @@ class ApplicationInstall extends Command
         $sGit = $this->ask('Which Github repository?');
         $sGitBranch = $this->ask('Which Branch?');
 
-        echo shell_exec("git clone -b $sGitBranch --single-branch $sGit /var/www/$sDomain/ 2>&1");
+        echo shell_exec("cd /var/www/$sDomain && git clone -b $sGitBranch $sGit 2>&1");
     }
 }
