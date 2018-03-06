a3c0760823fd77e1174add92c54080ba390c35b7
ben182 <moin@benjaminbortels.de>
2018-03-06 13:05:13 +0100

    mysql

--- app/Http/Helper.php
+++ app/Http/Helper.php
@@ -27,3 +27,25 @@ function getStringBetween($str,$from,$to)
     $sub = substr($str, strpos($str,$from)+strlen($from),strlen($str));
     return substr($sub,0,strpos($sub,$to));
 }
+
+function getConfig() {
+    return json_decode(file_get_contents(base_path('config.json')), TRUE);
+}
+
+function buildMysqlCommand($sCommand) {
+    $aMysql = getConfig()['mysql'];
+
+    $sMysqlUser = $aMysql['username'];
+    $sMysqlPassword = $aMysql['password'];
+
+    return shell_exec("mysql -u $sMysqlUser -p\"$sMysqlPassword\" -e \"$sCommand\"");
+}
+
+function createMysqlDatabase($sDatabase) {
+
+    echo buildMysqlCommand("CREATE DATABASE $sDatabase;");
+}
+
+function createMysqlUserAndGiveAccessToDatabase() {
+
+}
