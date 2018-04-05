4abf500e3721dc905bd37ff0c7f2b129356cf7c7
Benjamin Bortels <benjamin.bortels@ggh-mullenlowe.de>
2018-04-05 17:29:33 +0200

    Snapshots

--- app/Console/Commands/SnapshotBackupExecute.php
+++ app/Console/Commands/SnapshotBackupExecute.php
@@ -4,14 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class SnapshotBackupSetup extends Command
+class SnapshotBackupExecute extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'snapshot:setup';
+    protected $signature = 'snapshot:backup {keep}';
 
     /**
      * The console command description.
@@ -37,13 +37,17 @@ class SnapshotBackupSetup extends Command
      */
     public function handle()
     {
-        $sToken = $this->secret('What is your DigitalOcean API Token?');
-        shell_exec("export DIGITAL_OCEAN_ACCESS_TOKEN=$sToken");
-        shell_exec('echo "DOAT=\"' . encrypt($sToken) . '\"" >> /etc/environment');
+        $iKeep = $this->argument('keep');
+        $iDropletId = getenv('DROPLET_ID');
+        if (!$iDropletId) {
+            return false;
+        }
 
-        $iKeep = (int) $this->ask('How much snapshots to keep?');
+        $sToken = decrypt(getenv('DOAT'));
+        if (!$sToken) {
+            return false;
+        }
 
-        echo shell_exec('gem install do_snapshot');
-        echo shell_exec('crontab -l | { cat; echo "0 0 * * * server-tools snapshot:backup ' . $iKeep . ' >> /dev/null 2>&1"; } | crontab -');
+        shell_exec("/usr/local/bin/do_snapshot --only ' . $iDropletId . ' -k ' . $iKeep . ' -c -q --digital-ocean-access-token " . $sToken);
     }
 }
