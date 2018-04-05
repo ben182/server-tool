a908e923e2a19c64b2bd735c6c3ffda6a3bb3ffc
Benjamin Bortels <benjamin.bortels@ggh-mullenlowe.de>
2018-04-05 11:19:58 +0200

    Version Check

---
+++ app/Console/Commands/VersionCheck.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class VersionCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'version:check {app}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $sApp = $this->argument('app');
+        call_user_func([$this, $sApp]);
+    }
+
+    protected function nodejs()
+    {
+        $sRemoteNodejsVersion = shell_exec('curl -s semver.io/node/stable');
+        $sLocalNodejsVersion = shell_exec('nvm current');
+        echo $sRemoteNodejsVersion;
+        echo $sLocalNodejsVersion;
+    }
+}
