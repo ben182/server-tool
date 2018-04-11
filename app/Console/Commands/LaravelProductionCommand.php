2c6fcd666b7e225e558dcd46cc0f6f6f063d79d4
Benjamin Bortels <moin@benjaminbortels.de>
2018-04-12 00:37:37 +0200

    Laravel Production

---
+++ app/Console/Commands/LaravelProductionCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\ModCommand;
+use Illuminate\Console\Command;
+
+class AddVhostCommand extends ModCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laravel:production';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo shell_exec('bash ' . scripts_path() . 'laravel.production.sh');
+    }
+}
---
+++ scripts/laravel.production.sh
@@ -0,0 +1,8 @@
+php artisan cache:clear
+php artisan view:clear
+
+php artisan view:cache
+php artisan config:cache
+php artisan route:cache
+
+npm run prod
