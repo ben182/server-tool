6e542f0af7debb30982be09b7b6c48a7432d747a
Benjamin Bortels <moin@benjaminbortels.de>
2018-07-15 18:41:16 +0200

    Yarn

--- scripts/install.sh
+++ scripts/install.sh
@@ -203,6 +203,16 @@ nodeInstall () {
 echo "Installing Node JS..."
 nodeInstall &> /dev/null
 
+# YARN
+yarnInstall () {
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt-get update -y
+    sudo apt-get install yarn -y
+}
+echo "Installing Yarn..."
+yarnInstall &> /dev/null
+
 # REDIS
 redisInstall () {
     apt-get install build-essential tcl -y
