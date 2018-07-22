34a6dede052c50d3e248e75f375ba68bc91b9228
Benjamin Bortels <moin@benjaminbortels.de>
2018-07-22 21:19:51 +0200

    Updated nvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -190,7 +190,7 @@ sshKey &> /dev/null
 
 # NODE
 nodeInstall () {
-    curl -o- -sS https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+    curl -o- -sS https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
