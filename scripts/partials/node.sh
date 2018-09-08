76c416323d6319e69f2bdf2f2fa10269cf06ec27
Benjamin Bortels <moin@benjaminbortels.de>
2018-09-08 21:50:42 +0200

    new node installer

--- scripts/partials/node.sh
+++ scripts/partials/node.sh
@@ -4,13 +4,16 @@ source /etc/stool/scripts/helper.sh
 
 # NODE
 nodeInstall () {
-    curl -o- -sS https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    # curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+    # curl -o- -sS https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    # export NVM_DIR="$HOME/.nvm"
+    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-    nvm install node
-    nvm use node
+    # nvm install node
+    # nvm use node
     # n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
 }
 echo "Installing Node JS..."
