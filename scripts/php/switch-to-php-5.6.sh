913153249fc395731b90d405646f260cf0822d6d
Benjamin Bortels <moin@benjaminbortels.de>
2018-09-19 12:39:39 +0200

    PHP Version Switch enable & disable Service

--- scripts/php/switch-to-php-5.6.sh
+++ scripts/php/switch-to-php-5.6.sh
@@ -2,15 +2,19 @@
 
 echo "* Disabling Apache PHP 7.0 module..."
 sudo a2disconf php7.0-fpm > /dev/null
+sudo service php7.0-fpm stop
 
 echo "* Disabling Apache PHP 7.1 module..."
 sudo a2disconf php7.1-fpm > /dev/null
+sudo service php7.1-fpm stop
 
 echo "* Disabling Apache PHP 7.2 module..."
 sudo a2disconf php7.2-fpm > /dev/null
+sudo service php7.2-fpm stop
 
 echo "* Enabling Apache PHP 5.6 module..."
 sudo a2enconf php5.6-fpm > /dev/null
+sudo service php5.6-fpm start
 
 echo "* Restarting Apache..."
 sudo service apache2 restart > /dev/null
