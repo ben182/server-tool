a0c4cf34450575aed25bef03360d40cdbea0b70d
Benjamin Bortels <moin@benjaminbortels.de>
2018-10-22 10:40:45 +0200

    User Management

--- scripts/partials/user.sh
+++ scripts/partials/user.sh
@@ -6,6 +6,15 @@ source /etc/stool/scripts/helper.sh
 userManagement () {
     sudo cp ${TEMPLATES_PATH}sudoers.d/stool /etc/sudoers.d/stool
     sudo adduser --disabled-password --gecos "" stool
+
+    echo "AllowUsers stool root" >> /etc/ssh/sshd_config
+    sudo systemctl restart ssh
+
+    sudo -Hu stool ssh-keygen -f "/home/stool/.ssh/id_rsa" -t rsa -b 4096 -N ''
+    cp /root/.ssh/authorized_keys /home/stool/.ssh/
+
+    sudo chown -R stool:stool /home/stool/.ssh/
+    chmod 600 /home/stool/.ssh/authorized_keys
 }
 echo "User Management..."
 userManagement &> /dev/null
