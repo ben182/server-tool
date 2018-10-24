5bd38a057bfeccaf3f42c41f7ebd562db4ece41a
Benjamin Bortels <benjamin.bortels@ggh-mullenlowe.de>
2018-10-24 14:04:09 +0200

    Bash Aliases & SSH Port Fix

--- scripts/partials/user.sh
+++ scripts/partials/user.sh
@@ -7,11 +7,13 @@ userManagement () {
     sudo cp ${TEMPLATES_PATH}sudoers.d/stool /etc/sudoers.d/stool
     sudo adduser --disabled-password --gecos "" stool
 
+    sudo cp ${TEMPLATES_PATH}.bash_aliases /home/stool/.bash_aliases
+
     echo "AllowUsers stool root" >> /etc/ssh/sshd_config
 
     # SSH PORT
     echo "Port 12920" >> /etc/ssh/sshd_config
-    sudo ufw allow 12920
+    sudo ufw allow 12920/tcp
 
     sudo systemctl restart ssh
 
