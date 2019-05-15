4b909655db67bbfa6f06c53cd6b8907e8fa36ed5
Benjamin Bortels <moin@benjaminbortels.de>
2019-05-15 14:59:30 +0200

    Clean Up

--- scripts/partials/finish.sh
+++ scripts/partials/finish.sh
@@ -47,7 +47,6 @@ finish () {
 echo "Finish..."
 finish
 
-# echo "Installation successfully completed in $((($(date +%s)-$start)/60)) minutes"
 echo "All sensitive data is written to $CONFIG_PATH"
 echo 'Important! Please log out of this ssh session and start a new one!'
 echo 'Type "stool installation:test" to test your installation'
