5f644c07f5a980cf5e97fda471f00cf41aeca059
Benjamin Bortels <moin@benjaminbortels.de>
2019-05-28 00:29:54 +0200

    wip

--- app/Console/Commands/WelcomeMessageCommand.php
+++ app/Console/Commands/WelcomeMessageCommand.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Console\Stool;
 
-class StoolVersionCommand extends Command
+class WelcomeMessageCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'version';
+    protected $signature = 'stool:welcome-message';
 
     /**
      * The console command description.
@@ -38,6 +38,7 @@ class StoolVersionCommand extends Command
      */
     public function handle()
     {
+        $this->line('This server was provisioned by stool | Developed by Benjamin Bortels');
         $this->line('stool v' . Stool::version());
     }
 }
