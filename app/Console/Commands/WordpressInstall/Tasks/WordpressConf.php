a188f7fd0c3f371cd32138a8cf8b44557db28e08
Benjamin Bortels <moin@benjaminbortels.de>
2019-06-04 23:20:12 +0200

    wip

--- app/Console/Commands/WordpressInstall/Tasks/WordpressConf.php
+++ app/Console/Commands/WordpressInstall/Tasks/WordpressConf.php
@@ -36,7 +36,7 @@ class WordpressConf extends Task
         }
 
         if ($this->options->installPlugins) {
-            $this->shell->exec("cd {$this->bindings->installationDir} && wp plugin install all-in-one-seo-pack all-in-one-wp-migration wp-smushit wordfence wps-hide-login --activate");
+            $this->shell->exec("cd {$this->bindings->installationDir} && wp plugin install wordpress-seo all-in-one-wp-migration wp-smushit wordfence wps-hide-login --activate");
 
             $this->shell->exec("cd {$this->bindings->installationDir} && wp plugin install w3-total-cache");
         }
