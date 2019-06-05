d80557d7d38e44e3f2fe478c3742e588e3833551
Benjamin Bortels <moin@benjaminbortels.de>
2019-06-05 17:36:23 +0200

    wip

--- app/Console/Commands/VhostBasicAuth/Tasks/VhostBasicAuthTask.php
+++ app/Console/Commands/VhostBasicAuth/Tasks/VhostBasicAuthTask.php
@@ -11,6 +11,7 @@ class VhostBasicAuthTask extends Task
 
     public function handle()
     {
+        $this->shell->exec("sudo mkdir -p /home/stool/.stool/{$this->bindings->domain}");
         $this->shell->exec("sudo touch /home/stool/.stool/{$this->bindings->domain}/.htpasswd");
         $this->shell->exec("sudo htpasswd -b /home/stool/.stool/{$this->bindings->domain}/.htpasswd {$this->options->user} {$this->options->password}");
     }
