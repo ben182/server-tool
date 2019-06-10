ca9f3877938e2613e1d4224e262b388094d3d5ec
Benjamin Bortels <moin@benjaminbortels.de>
2019-06-10 15:37:46 +0200

    wip

--- scripts/partials/netdata.sh
+++ scripts/partials/netdata.sh
@@ -9,12 +9,13 @@ HTACCESS_PASS=$(passwordgen);
 
 sudo apt-get install netdata -y
 
-cp ${TEMPLATES_PATH}apache/netdata.conf /etc/apache2/sites-available/netdata.conf
+sudo cp ${TEMPLATES_PATH}apache/netdata.conf /etc/apache2/sites-available/netdata.conf
 sudo sed -i "s|IP_HERE|$PUBLIC_IP|" /etc/apache2/sites-available/netdata.conf
-a2ensite netdata.conf
+sudo chmod -x /etc/apache2/sites-available/netdata.conf
+sudo a2ensite netdata.conf
 
-htpasswd -c -b /etc/netdata/.htpasswd $HTACCESS_USER $HTACCESS_PASS
+sudo htpasswd -c -b /etc/netdata/.htpasswd $HTACCESS_USER $HTACCESS_PASS
 sudo sed -i "s|NETDATA_HTACCESS_USERNAME|$HTACCESS_USER|" $CONFIG_PATH
 sudo sed -i "s|NETDATA_HTACCESS_PASSWORD|$HTACCESS_PASS|" $CONFIG_PATH
 
-service apache2 restart
+sudo service apache2 restart
