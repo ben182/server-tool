19d59d958976449b7350243ed24013a5d6b2fdd2
Benjamin Bortels <moin@benjaminbortels.de>
2019-06-10 17:26:21 +0200

    wip

--- scripts/partials/netdata.sh
+++ scripts/partials/netdata.sh
@@ -9,6 +9,13 @@ HTACCESS_PASS=$(passwordgen);
 
 sudo apt-get install netdata -y
 
+echo "        history = 43200" >> /etc/netdata/netdata.conf
+echo "        access log = none" >> /etc/netdata/netdata.conf
+
+sudo service netdata restart
+
+# APACHE PROXY
+
 sudo cp ${TEMPLATES_PATH}apache/netdata.conf /etc/apache2/sites-available/netdata.conf
 sudo sed -i "s|IP_HERE|$PUBLIC_IP|" /etc/apache2/sites-available/netdata.conf
 sudo chmod -x /etc/apache2/sites-available/netdata.conf
@@ -17,5 +24,7 @@ sudo a2ensite netdata.conf
 sudo htpasswd -c -b /etc/netdata/.htpasswd $HTACCESS_USER $HTACCESS_PASS
 sudo sed -i "s|NETDATA_HTACCESS_USERNAME|$HTACCESS_USER|" $CONFIG_PATH
 sudo sed -i "s|NETDATA_HTACCESS_PASSWORD|$HTACCESS_PASS|" $CONFIG_PATH
+
+# TODO: fix slash at the end
 
 sudo service apache2 restart
